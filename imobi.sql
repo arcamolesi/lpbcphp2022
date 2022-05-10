e3151d0ee1d26e1fd52324b0800677a834c0a04f
Almir Rogério Camolesi <camolesi@fema.edu.br>
2022-05-10 20:02:21 -0300

    Update imobi.sql
    ATualização Dados em Banco de Dados

--- imobi.sql
+++ imobi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 11-Maio-2022 às 00:55
+-- Tempo de geração: 11-Maio-2022 às 01:01
 -- Versão do servidor: 10.4.13-MariaDB
 -- versão do PHP: 7.4.8
 
@@ -20,8 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `imobi`
 --
-CREATE DATABASE IF NOT EXISTS `imobi` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `imobi`;
 
 -- --------------------------------------------------------
 
@@ -38,59 +36,15 @@ CREATE TABLE `imovel` (
   `status` varchar(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
 --
--- Estrutura da tabela `locacao`
+-- Extraindo dados da tabela `imovel`
 --
 
-DROP TABLE IF EXISTS `locacao`;
-CREATE TABLE `locacao` (
-  `id` int(11) NOT NULL,
-  `imovel` int(11) NOT NULL,
-  `locador` int(11) NOT NULL,
-  `data` date NOT NULL,
-  `valor` float NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `locador`
---
-
-DROP TABLE IF EXISTS `locador`;
-CREATE TABLE `locador` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(35) NOT NULL,
-  `cidade` varchar(25) NOT NULL,
-  `estado` varchar(2) NOT NULL,
-  `idade` int(11) NOT NULL,
-  `salario` float NOT NULL,
-  `profissao` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `profissao`
---
-
-DROP TABLE IF EXISTS `profissao`;
-CREATE TABLE `profissao` (
-  `id` int(11) NOT NULL,
-  `descricao` varchar(35) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Extraindo dados da tabela `profissao`
---
-
-INSERT INTO `profissao` (`id`, `descricao`) VALUES
-(1, 'Professor'),
-(2, 'Pro'),
-(3, 'Consultor'),
-(4, 'Analista');
+INSERT INTO `imovel` (`id`, `rua`, `bairro`, `cidade`, `status`) VALUES
+(1, 'Rua João Ramalho, 1585', 'Tenis Clube', 'Assis', 'L'),
+(2, 'Rua do Lago', 'Aguas Claras', 'Assis', 'L'),
+(3, 'Rua Candido Mota, 1234', 'FEMA', 'Assis', 'L'),
+(4, 'Rua XV de Novembro, 1353', 'Centro', 'Assis', 'L');
 
 --
 -- Índices para tabelas despejadas
@@ -103,27 +57,6 @@ ALTER TABLE `imovel`
   ADD PRIMARY KEY (`id`);
 
 --
--- Índices para tabela `locacao`
---
-ALTER TABLE `locacao`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `locacao_imovel` (`imovel`),
-  ADD KEY `locacao_locador` (`locador`);
-
---
--- Índices para tabela `locador`
---
-ALTER TABLE `locador`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `locador_profissao` (`profissao`);
-
---
--- Índices para tabela `profissao`
---
-ALTER TABLE `profissao`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT de tabelas despejadas
 --
 
@@ -131,42 +64,7 @@ ALTER TABLE `profissao`
 -- AUTO_INCREMENT de tabela `imovel`
 --
 ALTER TABLE `imovel`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `locacao`
---
-ALTER TABLE `locacao`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `locador`
---
-ALTER TABLE `locador`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `profissao`
---
-ALTER TABLE `profissao`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- Restrições para despejos de tabelas
---
-
---
--- Limitadores para a tabela `locacao`
---
-ALTER TABLE `locacao`
-  ADD CONSTRAINT `locacao_imovel` FOREIGN KEY (`imovel`) REFERENCES `imovel` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `locacao_locador` FOREIGN KEY (`locador`) REFERENCES `locador` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Limitadores para a tabela `locador`
---
-ALTER TABLE `locador`
-  ADD CONSTRAINT `locador_profissao` FOREIGN KEY (`profissao`) REFERENCES `profissao` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
