5421ef0f1c6e8a162fa4e7c55f2489ff8beb2820
Almir Rogério Camolesi <camolesi@fema.edu.br>
2022-05-14 15:43:35 -0300

    Pagina Inserir em Branco
    Atualizar página inserir em branco

--- lstImovel.php
+++ lstImovel.php
@@ -1,6 +1,6 @@
 <?php
    include 'conexao.php';
-   $pdo = conexao::conectar(); 
+   $pdo = Conexao::conectar(); 
    $sql = "select * from imovel order by rua;";
    $lstImovel = $pdo->query($sql); 
 ?>
